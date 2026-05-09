a2ebde000531f7c9254bdb14abe2adb2218c1160
Antonio Ruíz González <j.ant.r.gon@gmail.com>
2026-05-09 12:51:32 -0600

    implementar modelo de Configuraciones de Usuario y endpoints de API
    Modelado de Datos: Creación del modelo ConfiguracionPC con relaciones ForeignKey hacia el modelo de usuarios de Django y ManyToManyField hacia el catálogo de componentes.
    
    Lógica de Negocio: Implementación de campos de auditoría (fecha_creacion, fecha_actualizacion) para el seguimiento de ensambles personalizados.
    
    Exposición de API: Desarrollo de ConfiguracionPCSerializer y registro del ConfiguracionPCViewSet en el router de la API para habilitar operaciones CRUD en formato JSON.
    
    Persistencia: Generación y aplicación de la migración 0003_configuracionpc en la base de datos PostgreSQL.

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ComponenteListView, ComponenteDetailView, ComponenteViewSet
+from .views import ComponenteListView, ComponenteDetailView, ComponenteViewSet, ConfiguracionPCViewSet
+
 
 # Configuración del Router para la API
 router = DefaultRouter()
 router.register(r'componentes', ComponenteViewSet)
+router.register(r'configuraciones', ConfiguracionPCViewSet)
 
 urlpatterns = [
     # -----------------------------------
